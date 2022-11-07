0c5427473a68e3e5d01682bec5ee9a806142d4bf
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-11-07 22:50:53 +0200

    modified index js

--- src/index.js
+++ src/index.js
@@ -92,13 +92,6 @@ function countryCardMarkup(result) {
 }
 
 
-// countryInfo.onmousemove = (e) => {
-//     const x = e.pageX - e.target.offsetLeft
-//     const y = e.pageY - e.target.offsetTop
-//     e.target.style.setProperty('--x', `${x}px`)
-//     e.target.style.setProperty('--y', `${y}px`)
-
-// }
 
 
 
